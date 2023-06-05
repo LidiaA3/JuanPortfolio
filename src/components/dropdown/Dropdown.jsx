eddecd6c50c2db51054e8e104d198fbbded887f4
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-06-05 03:02:15 +0200

    Funcionalidad Dropdown Terminada

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,13 +1,45 @@
-import "./dropdown.scss";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import $ from "jquery";
 
+import "./dropdown.scss";
+
 function Dropdown() {
+  const [dropdownActive, setDropdownActive] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setDropdownActive(false); // Cierra el desplegable al cambiar de ubicación
+  }, [location]);
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      // Verificar si se hizo clic en el elemento .dropdown__category
+      if ($(event.target).hasClass("dropdown__category")) {
+        const listItem = $(event.target).closest("li");
+        listItem.find(".dropdown__projects-list").slideToggle();
+        listItem.find(".dropdown__category").toggleClass("active");
+        setDropdownActive((prevState) => !prevState);
+      }
+    };
+
+    $(".dropdown__accordion").on("click", handleClick);
+
+    return () => {
+      $(".dropdown__accordion").off("click", handleClick);
+    };
+  }, []);
+
   return (
     <>
       <div className="dropdown__container">
         <ul className="dropdown__accordion">
           <li>
-            <div className="h5 dropdown__category">
+            <div
+              className={`h5 dropdown__category ${
+                dropdownActive ? "active" : ""
+              }`}
+            >
               Diseño UX/UI<span className="dropdown__icon"></span>
             </div>
             <ul className="text-default dropdown__projects-list">
@@ -26,7 +58,11 @@ function Dropdown() {
             </ul>
           </li>
           <li>
-            <div className="h5 dropdown__category">
+            <div
+              className={`h5 dropdown__category ${
+                dropdownActive ? "active" : ""
+              }`}
+            >
               Diseño gráfico<span className="dropdown__icon"></span>
             </div>
             <ul className="text-default dropdown__projects-list">
@@ -45,7 +81,11 @@ function Dropdown() {
             </ul>
           </li>
           <li>
-            <div className="h5 dropdown__category">
+            <div
+              className={`h5 dropdown__category ${
+                dropdownActive ? "active" : ""
+              }`}
+            >
               Desarrollo web<span className="dropdown__icon"></span>
             </div>
             <ul className="text-default dropdown__projects-list">
@@ -64,8 +104,12 @@ function Dropdown() {
             </ul>
           </li>
           <li>
-            <div className="h5 dropdown__category">
-              Otros proyectos<span className="dropdown__icon"></span>
+            <div
+              className={`h5 dropdown__category ${
+                dropdownActive ? "active" : ""
+              }`}
+            >
+              Otros<span className="dropdown__icon"></span>
             </div>
             <ul className="text-default dropdown__projects-list">
               <li>
@@ -87,19 +131,5 @@ function Dropdown() {
     </>
   );
 }
-
-$(function () {
-  $(".dropdown__accordion li").click(function () {
-    $(this).children(".dropdown__projects-list").slideToggle();
-
-    if ($(this).children(".dropdown__category").hasClass("active")) {
-      $(this).children(".dropdown__category").removeClass("active");
-    } else {
-      $(this).children(".dropdown__category").addClass("active");
-    }
-
-    return false;
-  });
-});
 
 export default Dropdown;
