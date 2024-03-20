b5bdb04a8dd08a84c54e492a47cff4da9378c1da
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2024-03-20 13:37:04 +0100

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://juantamarit.github.io/project-gamma-00/tree/redesign",
+
 })
